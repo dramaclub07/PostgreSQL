a64cd51ff947f494bede9362406e3b5f3d848cde
dramaclub07 <priyanshupositive@gmail.com>
2025-01-15 15:38:11 +0530

    [Priyanshu] ADD: executed views in psql

---
+++ views/view.sql
@@ -0,0 +1,30 @@
+                                                                List of databases
+       Name       |  Owner   | Encoding | Locale Provider |      Collate       |       Ctype        | Locale | ICU Rules |   Access privileges   
+------------------+----------+----------+-----------------+--------------------+--------------------+--------+-----------+-----------------------
+ aggregate        | postgres | UTF8     | libc            | English_India.1252 | English_India.1252 |        |           | 
+ company          | postgres | UTF8     | libc            | English_India.1252 | English_India.1252 |        |           | =Tc/postgres         +
+                  |          |          |                 |                    |                    |        |           | postgres=CTc/postgres+
+                  |          |          |                 |                    |                    |        |           | abhinav=c/postgres
+ e_commerce       | postgres | UTF8     | libc            | English_India.1252 | English_India.1252 |        |           | 
+ library          | postgres | UTF8     | libc            | English_India.1252 | English_India.1252 |        |           | 
+ postgres         | postgres | UTF8     | libc            | English_India.1252 | English_India.1252 |        |           | 
+ staff            | postgres | UTF8     | libc            | English_India.1252 | English_India.1252 |        |           | 
+ template0        | postgres | UTF8     | libc            | English_India.1252 | English_India.1252 |        |           | =c/postgres          +
+                  |          |          |                 |                    |                    |        |           | postgres=CTc/postgres
+ template1        | postgres | UTF8     | libc            | English_India.1252 | English_India.1252 |        |           | =c/postgres          +
+                  |          |          |                 |                    |                    |        |           | postgres=CTc/postgres
+ university       | postgres | UTF8     | libc            | English_India.1252 | English_India.1252 |        |           | 
+ valorantsubquery | postgres | UTF8     | libc            | English_India.1252 | English_India.1252 |        |           | 
+(10 rows)
+
+CREATE VIEW
+CREATE VIEW
+UPDATE 0
+CREATE VIEW
+CREATE VIEW
+DROP VIEW
+CREATE VIEW
+ emp_name | dept_id | emp_salary 
+----------+---------+------------
+(0 rows)
+
