a9988f5bf7024f5356751415922913bfd8aa3426
dramaclub07 <priyanshupositive@gmail.com>
2025-01-15 14:05:23 +0530

    [Priyanshu] ADD: Added schema solution to  Subqueries problem statements

---
+++ Subqueries/subquery.sql
@@ -0,0 +1,77 @@
+                                                                List of databases
+       Name       |  Owner   | Encoding | Locale Provider |      Collate       |       Ctype        | Locale | ICU Rules |   Access privileges   
+------------------+----------+----------+-----------------+--------------------+--------------------+--------+-----------+-----------------------
+ aggregate        | postgres | UTF8     | libc            | English_India.1252 | English_India.1252 |        |           | 
+ company          | postgres | UTF8     | libc            | English_India.1252 | English_India.1252 |        |           | =Tc/postgres         +
+                  |          |          |                 |                    |                    |        |           | postgres=CTc/postgres+
+                  |          |          |                 |                    |                    |        |           | abhinav=c/postgres
+ e_commerce       | postgres | UTF8     | libc            | English_India.1252 | English_India.1252 |        |           | 
+ library          | postgres | UTF8     | libc            | English_India.1252 | English_India.1252 |        |           | 
+ postgres         | postgres | UTF8     | libc            | English_India.1252 | English_India.1252 |        |           | 
+ staff            | postgres | UTF8     | libc            | English_India.1252 | English_India.1252 |        |           | 
+ template0        | postgres | UTF8     | libc            | English_India.1252 | English_India.1252 |        |           | =c/postgres          +
+                  |          |          |                 |                    |                    |        |           | postgres=CTc/postgres
+ template1        | postgres | UTF8     | libc            | English_India.1252 | English_India.1252 |        |           | =c/postgres          +
+                  |          |          |                 |                    |                    |        |           | postgres=CTc/postgres
+ university       | postgres | UTF8     | libc            | English_India.1252 | English_India.1252 |        |           | 
+ valorantsubquery | postgres | UTF8     | libc            | English_India.1252 | English_India.1252 |        |           | 
+(10 rows)
+
+ emp_id | emp_name | emp_salary 
+--------+----------+------------
+      2 | Raman    |   34590.00
+      3 | Kalam    |   43600.00
+(2 rows)
+
+ emp_name 
+----------
+ Adarsh
+ Raman
+ Kalam
+ Ajay
+ Jyoti
+(5 rows)
+
+ emp_name 
+----------
+ Adarsh
+ Raman
+ Kalam
+ Ajay
+ Jyoti
+(5 rows)
+
+ cust_id | order_count 
+---------+-------------
+       1 |           3
+(1 row)
+
+ dept_name |     avg_salary     
+-----------+--------------------
+ HR        | 26900.000000000000
+ Marketing | 34590.000000000000
+ sales     | 43600.000000000000
+ Finance   | 33000.000000000000
+ Accounts  | 32000.000000000000
+(5 rows)
+
+ dept_id 
+---------
+     103
+(1 row)
+
+ dept_id 
+---------
+     103
+(1 row)
+
+ emp_name 
+----------
+(0 rows)
+
+ order_name | order_count 
+------------+-------------
+ Pan        |           2
+ M416       |           2
+(2 rows)
+
