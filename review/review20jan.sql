d777f68ccc426209e3559af56bb0948f68c4e3d4
dramaclub07 <priyanshupositive@gmail.com>
2025-01-20 13:35:57 +0530

    [Priyanshu] MOD: review 20 jan

--- review/review20jan.sql
+++ review/review20jan.sql
@@ -87,3 +87,51 @@ CREATE FUNCTION
 
 UPDATE 4
 CREATE FUNCTION
+
+ create procedure enrollstudent(en_id int, stu_id int,co_id int,p_grade decimal(3,2)) language plpgsql as $$
+review20jan$# begin
+review20jan$#     if not exists(select 1 from enrollments where studentid = stu_id and courseid = co_id) then
+review20jan$#         insert into enrollments(enrollmentid,studentid, courseid, grade)values(en_id, stu_id, co_id, p_grade);
+review20jan$#     else
+review20jan$#         if p_grade is not null then
+review20jan$#             update enrollments
+review20jan$#             set grade = p_grade
+review20jan$#             where studentid = stu_id and courseid = co_id;
+review20jan$#         end if;
+review20jan$#     end if;
+review20jan$# end $$;
+
+
+CREATE PROCEDURE
+
+review20jan=# SELECT * FROM enrollments;
+ enrollmentid | studentid  | courseid | grade
+--------------+------------+----------+-------
+            1 | 2110992350 |      111 |  9.90
+            2 | 2110991525 |      121 |  8.80
+            3 | 2110990046 |      123 |  7.50
+            4 | 2110990344 |      143 |  9.00
+            6 | 2110992350 |      123 |  9.00
+
+ select * from students where studentid = (select studentid from enrollments group by studentid having count(courseid) >= 2);
+ studentid  | firstname | lastname | enrollmentdate |               email
+------------+-----------+----------+----------------+-----------------------------------
+ 2110992350 | Priyanshu | Singh    | 2021-07-01     | priyanshu2350.be21@chitara.edu.in
+(1 row)
+
+
+select * from students where studentid = (select studentid from enrollments group by studentid having avg(grade) <8.8);
+ studentid  | firstname | lastname | enrollmentdate |              email
+------------+-----------+----------+----------------+---------------------------------
+ 2110990046 | Abhinav   | Lather   | 2021-07-01     | abhinav0046.be21@chitara.edu.in
+(1 row)
+
+
+create index idx_student_course on enrollments(studentid,courseid);
+CREATE INDEX
+
+review20jan=# select * from enrollments where studentid = 2110991525 and courseid = 121;
+ enrollmentid | studentid  | courseid | grade
+--------------+------------+----------+-------
+            2 | 2110991525 |      121 |  8.80
+(1 row)
